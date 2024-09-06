b8adaee493ed2e58a14f818c499f19748c55c59c
cankkun <nguyenduccanh177@gmail.com>
2024-09-06 20:42:18 +0700

    feat(Navbar,NavIcons,SearchBar,CartModal): Add Components

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SinglePage = (props: Props) => {
+  return (
+    <div>SinglePage</div>
+  )
+}
+
+export default SinglePage
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Menu from "./Menu";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
 
 
 const Navbar = () => {
   return (
-    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>Navbar</div>
-  )
-}
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      <div className="h-full flex items-center justify-between md:hidden">
+        {/* Mobile Screen */}
+        <Link href="/">
+          <div className="text-2xl tracking-wide">Cray</div>
+        </Link>
+        <Menu />
+      </div>
+      {/* Bigger Screen */}
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        {/* Left Screen */}
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
+          <Link href="/" className="flex items-center gap-3">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            <div className="text-2xl tracking-wide">Cray</div>
+          </Link>
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
+        </div>
+        {/* Right Screen */}
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavIcons />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
